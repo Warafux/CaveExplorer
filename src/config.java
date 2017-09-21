7bfc356016cbfee5d6c498f49d188c5ebf85f8b7
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2017-09-21 16:12:11 +0200

    More config variables
    -World and player config

--- src/config.java
+++ src/config.java
@@ -1,16 +1,29 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.AbstractMap.SimpleEntry;
 
 public class config {
+	//WORLD CONFIG
 	public static final int XSIZE = 5;
 	public static final int YSIZE = 5;
 	
-	List<SimpleEntry<slot, Integer>> availableSlots = new ArrayList<SimpleEntry<slot, Integer>>();
+	public static final double distanceBetweenSpawnExit = 3;
 	
+	List<SimpleEntry<slot, Integer>> availableSlots = new ArrayList<SimpleEntry<slot, Integer>>(
+		Arrays.asList(
+				new SimpleEntry<slot, Integer>(new wall(), 2),
+				new SimpleEntry<slot, Integer>(new floor(), 20)
+				)
+		);
+	
+	//PLAYER CONFIG
+	public String playerName;
 	public config() {
-		this.availableSlots.add(new SimpleEntry<slot, Integer>(new wall(), 2));
-		this.availableSlots.add(new SimpleEntry<slot, Integer>(new floor(), 20));
+		//WORLD CONFIG
+		
+		//PLAYER CONFIG
+		this.playerName = "undefined";
 	}
 	
 }
