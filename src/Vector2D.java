595d6fee76a688cff93a03e7b03527e3ac5d2bbe
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2017-09-21 16:10:27 +0200

    Vector2D distance method

--- src/Vector2D.java
+++ src/Vector2D.java
@@ -2,6 +2,10 @@
 public class Vector2D {
 	private int x = 0;
 	private int y = 0;
+	public static void main(String[] args) {
+		//TEST DISTANCE FUNCTION
+		//System.out.println(distance(new Vector2D(10,2), new Vector2D(99, -25)));
+	}
 	public Vector2D(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -12,5 +16,15 @@ public class Vector2D {
 	}
 	public int getY(){
 		return this.y;
+	}
+	
+	public static double distance(Vector2D v1, Vector2D v2) {
+		return Math.sqrt(
+			(
+			Math.pow(v1.getX() - v2.getX(), 2)
+				+
+			Math.pow(v1.getY() - v2.getY(), 2)
+			)
+		);
 	}
 }
