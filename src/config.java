060ee82d2e543970eba9aae868452532808d99ec
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2017-09-24 22:29:25 +0200

    Some config adjustments

--- src/config.java
+++ src/config.java
@@ -8,15 +8,16 @@ public class config {
 	public final int XSIZE = 10;
 	public final int YSIZE = 30;
 	
-	public final int MINPATHDISTANCE = 10;
-	public final int MAXPATHDISTANCE = 10;
+	public final int MAXFAILATTEMPTS = 100;
+	public final int MINPATHDISTANCE = 89;
+	public final int MAXPATHDISTANCE = 90;
 	
 	public static final double distanceBetweenSpawnExit = 3;
 	
 	List<SimpleEntry<slot, Integer>> availableSlots = new ArrayList<SimpleEntry<slot, Integer>>(
 		Arrays.asList(
-				new SimpleEntry<slot, Integer>(new wall(), 2),
-				new SimpleEntry<slot, Integer>(new floor(), 20)
+				new SimpleEntry<slot, Integer>(new wall(), 8),
+				new SimpleEntry<slot, Integer>(new floor(), 0)
 				)
 		);
 	
