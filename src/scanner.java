ba90d454cfc58328539c3a149a1a1df0e1deafac
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2017-09-29 06:15:34 +0200

    request WASD method

--- src/scanner.java
+++ src/scanner.java
@@ -8,4 +8,27 @@ public class scanner {
 		res = scanner.nextLine();
 		return res;
 	}
+	public static char requestWASDChar(){
+		String resLine = "";
+		char res;
+		while(resLine.equals("")){
+			resLine = scanner.nextLine();
+		}
+		res = resLine.charAt(0);
+		switch(res){
+		case 'w':
+		case 'W':
+		case 'a':
+		case 'A':
+		case 's':
+		case 'S':
+		case 'd':
+		case 'D':
+			return res;
+		default:
+			System.out.println("ERROR INVALID CHAR");
+			return requestWASDChar();
+		}
+		
+	}
 }
