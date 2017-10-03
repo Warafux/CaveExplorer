a4bbcb76a898f5ad6c3e60419064cb81a6879e51
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2017-10-03 02:59:35 +0200

    Item base redefined
    -Clonable
    -Use method
    -ItemName and usesLeft

--- src/item.java
+++ src/item.java
@@ -1,10 +1,33 @@
 
-public class item {
+public class item implements Cloneable{
 	char icon;
+	String itemName = "";
+	int usesLeft = -1;
 	public item(){
 		
+	}
+	public String getItemName(){
+		return this.itemName;
 	}
 	public char getIcon(){
 		return this.icon;
 	}
+	public void use(world world, player player){
+		
+	}
+	
+	//CLONABLE
+
+    @Override
+    public item clone() {
+    	item clone;
+        try {
+            clone = (item) super.clone();
+        }
+        catch (CloneNotSupportedException ex) {
+            throw new RuntimeException("Error cloning item", ex);
+        }
+
+        return clone;
+    }
 }
